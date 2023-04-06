afbd049915b6af69dc5784d6e72d7550c05a4312
nicolosanna <nicolosanna.main@gmail.com>
2023-04-06 19:01:25 +0200

    Changes by corrado.corda.s2_giu

--- src/pacchetto/ShipSelector.java
+++ src/pacchetto/ShipSelector.java
@@ -7,6 +7,7 @@ public class ShipSelector extends JFrame {
     Listener listener = new Listener(this);
 
     JPanel ships = new JPanel();
+    JPanel ship1 = new JPanel();
     JLabel ship2 = new JLabel();
     JLabel ship3= new JLabel();
     JLabel ship4 = new JLabel();
@@ -14,7 +15,21 @@ public class ShipSelector extends JFrame {
     public ShipSelector(){
         super("Ship Select");
 
-        JPanel ships
+        ships.setLayout(new GridLayout(2,2, 10,10));
+        ships.add(ship1);
+        ships.add(ship2);
+        ships.add(ship3);
+        ships.add(ship4);
 
+        c.add(ships);
+
+        this.addWindowListener(listener);
+        this.addMouseListener(listener);
+
+        this.setSize(250,250);
+        this.setLocation(ScreenSize.getWidth() / 2, ScreenSize.getHeight() / 2);
+        this.setResizable(false);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
     }
 }
