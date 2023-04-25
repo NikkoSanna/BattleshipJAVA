1af4599cd409a9e87589ea003f591a1ec68530d4
Corra05 <amacorra07@gmail.com>
2023-04-25 18:49:06 +0200

    .

--- src/packet/Listener.java
+++ src/packet/Listener.java
@@ -130,6 +130,8 @@ public class Listener extends JFrame implements ActionListener, WindowListener {
 
                 map.bottomBar.add(map.gameText);
                 map.gameText.setText("Questa e la tua mappa");
+
+
             }
         }catch (Exception e1){
             System.out.println("Errore nell'action performed di map");
