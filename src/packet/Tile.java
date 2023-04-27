60d248d7d8b37979fcd9ac57ab65ebfd55bb9ad8
Corra05 <amacorra07@gmail.com>
2023-04-27 21:00:17 +0200

    no non va

--- src/packet/Tile.java
+++ src/packet/Tile.java
@@ -221,6 +221,8 @@ public class Tile extends JButton implements MouseListener {
                         map.server.bufferOut.write(i + "," + j);
                         map.server.bufferOut.newLine();
                         map.server.bufferOut.flush();
+
+                        System.out.println("AAAAA");
                     } catch (IOException e1) {
                         throw new RuntimeException(e1);
                     }
